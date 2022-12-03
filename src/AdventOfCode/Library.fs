c1dc08b7cceb579b531830673190dc765a4524bf
Andrew Goacher <andrewgoacher@outlook.com>
2022-12-03 00:13:21 +0000

    Get test case working

--- src/AdventOfCode/Library.fs
+++ src/AdventOfCode/Library.fs
@@ -1,5 +1,41 @@
 ﻿namespace AdventOfCode
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+
+module Utils=
+    let split (chars: char[]) (input: string)=
+        input.Split(chars, StringSplitOptions.None)
+
+    let try_convert (input: string)=
+        let str = input.Replace("\r", "").Replace("\n", "") 
+        if String.IsNullOrEmpty(str) then
+            None
+        else
+            str
+            |> Convert.ToInt32 |> Some
+
+
+module Day1=
+    let private collect (list: int list) (item: int option)=
+        match list with
+        | head :: tail -> 
+            match item with
+            | None -> 0::list
+            | Some num -> (head + num)::tail
+        | [] -> 
+            match item with
+            | None -> list
+            | Some num -> [num]
+
+    let private get_value (item: int)=
+        item
+
+    let solve_day_1 (input: string)=
+        let list = 
+            input
+            |> Utils.split [|'\n'|]
+            |> Array.map Utils.try_convert
+            |> Array.fold collect List.empty
+            |> List.sortByDescending get_value
+
+        list.Head
--- tests/AdventOfCode.Tests/Tests.fs
+++ tests/AdventOfCode.Tests/Tests.fs
@@ -2,7 +2,26 @@ module Tests
 
 open System
 open Xunit
+open FsUnit
+open AdventOfCode
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Day 1 Part 1: Elf with most calories`` () =
+    let input = """
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+    let result = Day1.solve_day_1 input
+    result |> should equal 24000
